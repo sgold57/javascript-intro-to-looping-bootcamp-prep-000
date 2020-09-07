681918b985b654b9fae82ca792dce7b1be606d9c
carefree-request-1153 <sgold5757@gmail.com>
2020-09-07 16:25:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -11,3 +11,18 @@ function whileLoop(n) {
   }
   return "done"
 }
+
+function doWhileLoop(num) {
+  let i = 0;
+
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+
+  do{
+    console.log("I run once regardless.");
+  } while incrementVariable() < num;
+
+  return
+}
